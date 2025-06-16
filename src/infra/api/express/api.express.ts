595a28323e836a62148c7d362eb7d628a5047e52
Victor Siqueira <131918226+victorsz08@users.noreply.github.com>
2025-06-16 23:48:55 +0000

    api express

--- src/infra/api/express/api.express.ts
+++ src/infra/api/express/api.express.ts
@@ -1,3 +1,38 @@
 import { Api } from "../api";
+import express, { Express } from "express";
+import { Route } from "./routes/route.express";
+import CookieParser from "cookie-parser"
 
-export class ApiExpress implements Api {}
+export class ApiExpress implements Api {
+    private app: Express;
+
+    private constructor(routes: Route[]) {
+        this.app = express();
+
+        this.app.use(express.json());
+        this.app.use(CookieParser());
+
+        this.addRoutes(routes);
+    };
+
+    public static build(routes: Route[]) {
+        return new ApiExpress(routes);
+    };
+
+    private addRoutes(routes: Route[]) {
+        routes.forEach(route => {
+           const method = route.getMethod();
+           const handler = route.getHandler()
+           const path = route.getPath()
+           const middlewares = route.getMiddlewares();
+
+           this.app[method](path, ...middlewares, handler)
+        });
+    };
+
+    public start(port: number): void {
+        this.app.listen(port, () => {
+            console.log("http server is running in port: ", port)
+        })
+    }
+}
---
+++ src/infra/api/express/routes/route.express.ts
@@ -0,0 +1,21 @@
+type T = any;
+import { Request, Response, NextFunction } from "express";
+
+export type HttpMethod = "get" | "post" | "put" | "delete";
+export const HttpMethod = {
+    POST: "post" as HttpMethod,
+    GET: "get" as HttpMethod,
+    PUT: "put" as HttpMethod,
+    DELETE: "delete" as HttpMethod,
+} as const;
+
+export interface Route {
+    getHandler(): (req: Request, res: Response) => Promise<T>;
+    getPath(): string;
+    getMethod(): HttpMethod;
+    getMiddlewares(): ((
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ) => Promise<T>)[];
+}
