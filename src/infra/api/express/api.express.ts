3680795cc6ac2ba5f7684ef541b9bbd01c72637b
victorsz08 <joaovictorsal777@gmail.com>
2025-07-01 13:16:58 -0300

    fix credentials cors

--- src/infra/api/express/api.express.ts
+++ src/infra/api/express/api.express.ts
@@ -20,6 +20,7 @@ export class ApiExpress implements Api {
                 origin: process.env.ORIGIN,
                 allowedHeaders: ["Content-Type", "Authorization"],
                 methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+                credentials: true,
             })
         );
 
