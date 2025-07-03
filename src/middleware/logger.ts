a4c7906c898ffaa7525ad34158bdfcbf43072d02
victorsz08 <joaovictorsal777@gmail.com>
2025-07-03 09:02:41 -0300

    fix session route

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { config } from "../../prisma/config/config";
 import { verify } from "jsonwebtoken";
-import { UserDto } from "../package/mapper/user-mapper";
-
 export function Logger() {
     return async (req: Request, res: Response, next: NextFunction) => {
         const token = req.cookies["nt.authtoken"];
